2ab433c037431b0ec0bb2bf93ae21209ab53367e
takamitsu-iida <takamitsu.iida@gmail.com>
2025-09-08 22:15:17 +0900

    update

---
+++ tig/start.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# cfg/frr.conf is mounted directly to /etc/frr/frr.conf, see CML node definition file.
+CONFIG=/etc/frr/frr.conf
+
+BOOT=/config/boot.sh
+PROTOCOLS=/config/protocols
+
+# Not needed for Docker
+# for iface in /sys/class/net/*; do
+#   iface_name=$(basename "$iface")
+#   if /usr/sbin/ethtool "$iface_name" &>/dev/null; then
+#     /usr/sbin/ethtool -K "$iface_name" tx off
+#   fi
+# done
+
+#
+# run snmpd in the background
+#
+if [ -x /usr/sbin/snmpd ]; then
+    /usr/sbin/snmpd -LS 5 d -Lf /dev/null
+fi
+
+#
+# run sshd in the background
+#
+if [ -x /usr/sbin/sshd ]; then
+    if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+        ssh-keygen -A
+    fi
+
+    mkdir -p /var/run/sshd
+
+    # echo "cisco:cisco" | chpasswd
+    # echo "root:cisco" | chpasswd
+    passwd -d root
+
+    /usr/sbin/sshd -E /var/log/sshd.log
+fi
+
+# enable the requested protocols
+while IFS= read -r line; do
+    line=$(echo "$line" | xargs) # no whitespace
+    if [[ -n "$line" && ! "$line" =~ ^# ]]; then
+        sed -r -e "s/^(${line}=)no$/\1yes/" -i /etc/frr/daemons
+    fi
+done <"$PROTOCOLS"
+
+# Not needed to copy the day0 config as it's mounted directly to /etc/frr/frr.conf
+#if [ -f $CONFIG ]; then
+#    cp $CONFIG /etc/frr/frr.conf
+#fi
+
+# set the hostname from the provided config if it's there
+if [ -f $CONFIG ]; then
+    hostname_value="router"
+    if grep -q "^hostname" $CONFIG; then
+        hostname_value=$(awk '/^hostname/ {print $2}' $CONFIG)
+    fi
+    hostname $hostname_value
+fi
+
+# exit if frrinit.sh is not found
+if [ ! -f /usr/lib/frr/frrinit.sh ]; then
+    echo "/usr/lib/frr/frrinit.sh not found. Exiting."
+    exit 1
+fi
+
+# start frr
+/usr/lib/frr/frrinit.sh start
+
+echo "READY" >/dev/console
+
+trap '' INT TSTP
+while true; do
+    /usr/bin/vtysh
+done

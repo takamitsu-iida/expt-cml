a02ea86fcd7bb60978f530bcb9188fca765c217e
takamitsu-iida <takamitsu.iida@gmail.com>
2025-09-09 15:56:21 +0900

    update

--- tig/start.sh
+++ tig/start.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-# hostname ${HOSTNAME}
+hostname ${HOSTNAME}
 
 #
 # run sshd in the background
 #
 
-# if [ -x /usr/sbin/sshd ]; then
-#     if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-#         ssh-keygen -A
-#     fi
-#
-#     mkdir -p /var/run/sshd
-#
-#     # echo "cisco:cisco" | chpasswd
-#     # echo "root:cisco" | chpasswd
-#     passwd -d root
-#
-#     /usr/sbin/sshd -E /var/log/sshd.log
-# fi
+if [ -x /usr/sbin/sshd ]; then
+    if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+        ssh-keygen -A
+    fi
+    mkdir -p /var/run/sshd
+    # echo "cisco:cisco" | chpasswd
+    # echo "root:cisco" | chpasswd
+    passwd -d root
+    /usr/sbin/sshd -E /var/log/sshd.log
+fi
 
 #
 # run supervisord
 #
 
-# /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 
 echo "READY" >/dev/console
 

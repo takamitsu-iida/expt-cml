916892eb13217620b705ab80941c693d7e614263
takamitsu-iida <takamitsu.iida@gmail.com>
2025-09-10 13:45:22 +0900

    add script

---
+++ frr/cml_install_image.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+NODE_DEF_ROOT=/var/lib/libvirt/images/node-definitions
+NODE_DEF_FILENAME=frr-10-4.yaml
+
+IMAGE_DEF_ROOT=/var/lib/libvirt/images/virl-base-images
+IMAGE_DEF_DIR=frr-10-4
+IMAGE_DEF_FILENAME=frr-10-4.yaml
+IMAGE_NAME=frr.tar.gz
+
+if [ -f /var/tmp/node_definition.yaml ]; then
+    mv /var/tmp/node_definition.yaml ${NODE_DEF_ROOT}/${NODE_DEF_FILENAME}
+    chown libvirt-qemu:virl2 ${NODE_DEF_ROOT}/${NODE_DEF_FILENAME}
+else
+    echo "node_definition.yaml not found."
+fi
+
+mkdir -p ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}
+
+if [ -f /var/tmp/image_definition.yaml ]; then
+    mv /var/tmp/image_definition.yaml ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}/${IMAGE_DEF_FILENAME}
+else
+    echo "image_definition.yaml not found."
+fi
+
+if [ -f /var/tmp/${IMAGE_NAME} ]; then
+    mv /var/tmp/${IMAGE_NAME} ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}/${IMAGE_NAME}
+else
+    echo "${IMAGE_NAME} not found."
+fi
+
+chown -R libvirt-qemu:virl2 ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}
+
+# スクリプト自身を削除
+rm -- "$0"

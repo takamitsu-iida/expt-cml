8379dbf9f66db762ea92d7c47e9351c5acca51ed
takamitsu-iida <takamitsu.iida@gmail.com>
2025-09-10 13:40:08 +0900

    update

--- tig/cml_install_image.sh
+++ tig/cml_install_image.sh
@@ -8,10 +8,25 @@ IMAGE_DEF_DIR=tig
 IMAGE_DEF_FILENAME=tig.yaml
 IMAGE_NAME=tig.tar.gz
 
-mv /var/tmp/node_definition.yaml ${NODE_DEF_ROOT}/${NODE_DEF_FILENAME}
-chown libvirt-qemu:virl2 ${NODE_DEF_ROOT}/${NODE_DEF_FILENAME}
+if [ -f /var/tmp/node_definition.yaml ]; then
+    mv /var/tmp/node_definition.yaml ${NODE_DEF_ROOT}/${NODE_DEF_FILENAME}
+    chown libvirt-qemu:virl2 ${NODE_DEF_ROOT}/${NODE_DEF_FILENAME}
+else
+    echo "node_definition.yaml not found."
+fi
 
 mkdir -p ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}
-mv /var/tmp/image_definition.yaml ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}/${IMAGE_DEF_FILENAME}
-mv /var/tmp/${IMAGE_NAME} ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}/${IMAGE_NAME}
+
+if [ -f /var/tmp/image_definition.yaml ]; then
+    mv /var/tmp/image_definition.yaml ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}/${IMAGE_DEF_FILENAME}
+else
+    echo "image_definition.yaml not found."
+fi
+
+if [ -f /var/tmp/${IMAGE_NAME} ]; then
+    mv /var/tmp/${IMAGE_NAME} ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}/${IMAGE_NAME}
+else
+    echo "${IMAGE_NAME} not found."
+fi
+
 chown -R libvirt-qemu:virl2 ${IMAGE_DEF_ROOT}/${IMAGE_DEF_DIR}

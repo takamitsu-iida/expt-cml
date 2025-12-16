74d402342fc5a729029a44c7d16f6344bb3e3385
takamitsu-iida <takamitsu.iida@gmail.com>
2025-12-16 18:03:24 +0900

    update

--- arcos/nc.py
+++ arcos/nc.py
@@ -145,6 +145,7 @@ def apply_xml_config(config_file: str = OUTPUT_FILE):
 
         # <config xmlns="http://tail-f.com/ns/config/1.0"> ルート要素で囲む
         xml_config = f'<config xmlns="http://tail-f.com/ns/config/1.0">{xml_config_content}</config>'
+        xml_config = f'<nc:config xmlns:nc="urn:ietf:params:xml:ns:netconf:base:1.0">{xml_config}</nc:config>'
 
         print(f"➡️ NETCONF接続を試行中: {TARGET_HOST}:{TARGET_PORT} (ユーザー: {TARGET_USER})")
 

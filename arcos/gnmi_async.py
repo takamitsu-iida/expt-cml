e68213d34c1a4f44d3d67d42a2b857f162865d62
takamitsu-iida <takamitsu.iida@gmail.com>
2025-12-17 11:55:43 +0900

    update

--- arcos/gnmi_async.py
+++ arcos/gnmi_async.py
@@ -389,4 +389,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Program interrupted by user.")
